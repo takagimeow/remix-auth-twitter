5737516820cfa62ff0f1581803e8d5a787e952ee
Keisuke Takagi <mit@takagi.app>
2022-02-14 14:22:47 +0900

    chore: formatted

--- app/utils/twitter/getCurrentUser.server.ts
+++ app/utils/twitter/getCurrentUser.server.ts
@@ -1,19 +1,24 @@
 import TwitterApi from "twitter-api-v2";
 
-export async function getCurrentUser(accessToken: string, accessSecret: string) {
-	const twitterApiKey = process.env.TWITTER_API_KEY ?? "";
-	const twitterApiKeySecret = process.env.TWITTER_API_KEY_SECRET ?? "";
-	try {
-		const client = new TwitterApi({
-			appKey: twitterApiKey,
-			appSecret: twitterApiKeySecret,
-			accessToken, // oauth token from previous step (link generation)
-			accessSecret // oauth token secret from previous step (link generation)
-		});
-
-		const currentUser = await client.currentUser();
-		return currentUser;
-	} catch (error) {
-		return null;
-	}
+export async function getCurrentUser(
+  accessToken: string,
+  accessSecret: string
+) {
+  const twitterApiKey = process.env.TWITTER_API_KEY ?? "";
+  const twitterApiKeySecret = process.env.TWITTER_API_KEY_SECRET ?? "";
+  try {
+    const client = new TwitterApi({
+      appKey: twitterApiKey,
+      appSecret: twitterApiKeySecret,
+      accessToken, // oauth token from previous step (link generation)
+      accessSecret, // oauth token secret from previous step (link generation)
+    });
+    const currentUser = await client.currentUser();
+    return currentUser;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+    return null;
+  }
 }
